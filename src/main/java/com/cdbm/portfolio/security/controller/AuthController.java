928c618d940ce81a08c711d82d98fea012914987
carlosmorales1908 <80941723+carlosmorales1908@users.noreply.github.com>
2022-10-31 23:28:12 -0300

    CrossOrigin a heroku

--- src/main/java/com/cdbm/portfolio/security/controller/AuthController.java
+++ src/main/java/com/cdbm/portfolio/security/controller/AuthController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-frontend-aed6f.web.app/")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
